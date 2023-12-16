27deb595a35f90b31102878c2450060e6a906c03
THEALEXSAND <gohangaymer888@outlook.es>
2023-12-16 13:52:57 -0400

    First Commit 1.0v

---
+++ script.js
@@ -0,0 +1,36 @@
+const skillsContainer = document.getElementById("skills");
+
+const createSkillCard = ({ name, image }) => {
+	return `
+            <article class="skills__card">
+                <img src="img/${image}" alt="Logo ${name}">
+                <h4>${name}</h4>
+            </article>
+            `;
+};
+
+const skillCards = [
+	{ name: "HTML", image: "html-logo.png" },
+	{ name: "CSS", image: "css-logo.png" },
+	{ name: "JavaScript", image: "javascript-logo.png" },
+	{ name: "Node.js", image: "node-js.png" },
+	{ name: "SASS", image: "sass-logo.png" },
+];
+
+skillCards.forEach((skillCard) => {
+	skillsContainer.innerHTML += createSkillCard(skillCard);
+});
+
+const header = document.querySelector("header");
+
+const addHeaderShadow = () => {
+	if (window.scrollY > 0) {
+		header.classList.add("header__shadow");
+	} else {
+		header.classList.remove("header__shadow");
+	}
+};
+
+window.addEventListener("scroll", () => {
+	addHeaderShadow();
+});
